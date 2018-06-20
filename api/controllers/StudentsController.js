591b5fd08ae3ad79b38b21082994d0cd1aef0e9b
rajan sharma <rajansharma9697@gmail.com>
2018-06-20 14:31:38 +0530

    few changes

--- api/controllers/StudentsController.js
+++ api/controllers/StudentsController.js
@@ -1,88 +1,101 @@
 
+function addStudent(req, res) {
+  res.view('pages/addStudent');
+};
+
+
+function homepage(req, res) {
+
+  Students.find({}).exec((err, students) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+
+    res.view('pages/homepage', { students: students });
+
+  });
+
+};
+function deleteStudent(req, res) {
+
+  Students.find({}).exec((err, students) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+
+    res.view('pages/deleteStudent', { students: students });
+
+  });
+
+};
+function updateStudent(req, res) {
+
+  Students.find({}).exec((err, students) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+
+    res.view('pages/updateStudent', { students: students });
+
+  });
+};
+
+function createStudent(req, res) {
+
+  var name = req.body.fullname;
+  Students.create({ name: name }).exec((err) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+
+    res.redirect('/');
+
+  });
+};
+function deleteEntry(req, res) {
+  Students.destroy({ id: req.params.id }).exec((err) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+
+    res.redirect('/');
+
+  });
+};
+
+function updateInterim(req, res) {
+  Students.findOne({ id: req.params.id }).exec((err, students) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+    res.view('pages/studentInfo', { students: students });
+
+
+  });
+};
+function update(req, res) {
+  Students.update({ id: req.params.id }, { name: req.body.fullname }).exec((err) => {
+    if (err) {
+      res.send(500, { error: 'Database Error Occured' });
+    }
+    res.redirect('/');
+
+
+  });
+};
+
 
 module.exports = {
 
+  homepage: homepage,
+  deleteStudent: deleteStudent,
+  updateStudent: updateStudent,
+  createStudent: createStudent,
+  delete: deleteEntry,
+  updateInterim: updateInterim,
+  update: update,
+  addStudent: addStudent,
 
-  homepage: function (req, res) {
-
-    Students.find({}).exec((err, students) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-
-      res.view('pages/homepage', { students: students });
-
-    });
-
-  },
-  deleteStudent: function (req, res) {
-
-    Students.find({}).exec((err, students) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-
-      res.view('pages/deleteStudent', { students: students });
-
-    });
-
-  },
-  updateStudent: function (req, res) {
-
-    Students.find({}).exec((err, students) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-
-      res.view('pages/updateStudent', { students: students });
-
-    });
-  },
-  addStudent: function (req, res) {
-    res.view('pages/addStudent');
-  },
-  createStudent: function (req, res) {
-
-    var name = req.body.fullname;
-    Students.create({ name: name }).exec((err) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-
-      res.redirect('/');
-
-    });
-  },
-  delete: function (req, res) {
-    Students.destroy({ id: req.params.id }).exec((err) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-
-      res.redirect('/');
-
-    });
-  },
-  updateInterim: function (req, res) {
-    Students.findOne({ id: req.params.id }).exec((err, students) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-      res.view('pages/studentInfo', { students: students });
-      
-
-    });
-  },
-  update: function (req, res) {
-    Students.update({id: req.params.id},{name: req.body.fullname }).exec((err) => {
-      if (err) {
-        res.send(500, { error: 'Database Error Occured' });
-      }
-      res.redirect('/');
-      
-
-    });
-  },
 
 
 };
